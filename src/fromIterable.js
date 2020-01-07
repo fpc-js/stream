97a75a090ed6088d9db728ee270208958b96555b
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2020-01-07 14:03:49 +0100

    refactor: fromIterable

--- src/fromIterable.js
+++ src/fromIterable.js
@@ -2,20 +2,7 @@ import { expectIterable } from '@fpc/types';
 import { getIterator } from './internals';
 import { Stream } from './Stream';
 
-/* eslint-disable-next-line func-style */
-function *iterableGenerator (iterable) {
-  const iterator = getIterator(iterable);
-
-  while (true) {
-    const { done, value } = iterator.next();
-
-    if (done) {
-      break;
-    } else {
-      yield value;
-    }
-  }
-}
-
-export const fromIterable = iterable =>
-  new Stream(iterableGenerator, expectIterable(iterable));
+/* eslint-disable no-sequences */
+export const fromIterable = iterable => (
+  expectIterable(iterable), new Stream(() => getIterator(iterable))
+);
